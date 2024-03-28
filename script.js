13744504fc7090849c09d183289f68a3ad00f5c2
aalmash <almash050496@gmail.com>
2024-03-28 22:18:41 +0700

    added hw-9 and half of additional hw-9

--- script.js
+++ script.js
@@ -2,39 +2,50 @@ const buttonElement = document.getElementById("add-button");
 const listElement = document.getElementById("list");
 const nameInputElement = document.getElementById("name-input");
 const textInputElement = document.getElementById("text-input");
+//const deleteButtonElement = document.getElementById("delete-button");
+
+textInputElement.addEventListener('keyup', function (e) {
+  if (e.key === 'Enter') {
+    buttonElement.click();
+  }
+});
 
 buttonElement.addEventListener(('click'), () => {
-    const oldListElement = listElement.innerHTML;
 
-    const plus0 = (el) => {
-        if (el < 10) {
-            return el = '0' + el;
-        } else {
-            return el;
-        }
+  const oldListElement = listElement.innerHTML;
+
+
+  const plus0 = (el) => {
+    if (el < 10) {
+      return el = '0' + el;
+    } else {
+      return el;
     }
+  }
 
-    let currentDate = new Date();
-    let date = plus0(currentDate.getDate());
-    let month = plus0(currentDate.getMonth() + 1);
-    let year = plus0(currentDate.getFullYear());
-    let hour = plus0(currentDate.getHours());
-    let minute = plus0(currentDate.getMinutes());
+  let currentDate = new Date();
+  let date = plus0(currentDate.getDate());
+  let month = plus0(currentDate.getMonth() + 1);
+  let year = plus0(currentDate.getFullYear());
+  let hour = plus0(currentDate.getHours());
+  let minute = plus0(currentDate.getMinutes());
 
 
 
-    nameInputElement.classList.remove("error");
-    if (nameInputElement.value === "") {
-        nameInputElement.classList.add("error");
-        return;
-    }
-    textInputElement.classList.remove("error");
-    if (textInputElement.value === "") {
-        textInputElement.classList.add("error");
-        return;
-    }
+  nameInputElement.classList.remove("error");
+  if (nameInputElement.value === "") {
+    nameInputElement.classList.add("error");
+    buttonElement.disabled = true;
+    return;
+  }
+  textInputElement.classList.remove("error");
+  if (textInputElement.value === "") {
+    textInputElement.classList.add("error");
+    buttonElement.disabled = true;
+    return;
+  }
 
-    listElement.innerHTML = oldListElement + `<li class="comment">
+  listElement.innerHTML = oldListElement + `<li class="comment">
     <div class="comment-header">
       <div>${nameInputElement.value}</div>
       <div>${date}.${month}.${year} ${hour}:${minute}</div>
@@ -55,3 +66,6 @@ buttonElement.addEventListener(('click'), () => {
   nameInputElement.value = "";
   textInputElement.value = "";
 })
+
+
+//deleteButtonElement.addEventListener(('click'), () => {})
